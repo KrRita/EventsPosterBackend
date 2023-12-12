6830e8e14e24ace4e2f6d5d4a3f8e503b4b445be
KrRita <kravtsovarita2003@mail.ru>
2023-12-12 11:44:45 +0300

    Initial commit

---
+++ src/main/java/com/example/EventsPoster/EventsPosterApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventsPoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsPosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsPosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventsPoster/EventsPosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventsPoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsPosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
